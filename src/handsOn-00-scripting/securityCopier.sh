e4caa38d67edfb6cacf4bd421c7ed6a2cd4d5cf7
polMarsol <polmarsoluni@gmail.com>
2024-09-22 21:40:08 +0200

    update: security copier

--- src/handsOn-00-scripting/securityCopier.sh
+++ src/handsOn-00-scripting/securityCopier.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-SOURCE_DIR="C:\Users\Pol Marsol\OneDrive\Escritorio\UNIVERSITAT"
+SOURCE_DIR="" # Directori d'origen de la còpia de seguretat
+BACKUP_DIR="" # Directori de destí de la còpia de seguretat
 
-BACKUP_DIR="G:\Mi unidad\Universitat copia"
+echo "SOURCE_DIR: $SOURCE_DIR"
+echo "BACKUP_DIR: $BACKUP_DIR"
 
 DATE=$(date +'%Y-%m-%d_%H-%M-%S')
-
 BACKUP_FILE="backup_$DATE.tar.gz"
 
-tar -czf $BACKUP_DIR/$BACKUP_FILE $SOURCE_DIR
+tar -czf "$BACKUP_DIR/$BACKUP_FILE" "$SOURCE_DIR"
 
 if [ $? -eq 0 ]; then
     echo "Còpia de seguretat completada: $BACKUP_DIR/$BACKUP_FILE"
